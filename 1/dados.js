0ed09b05451eb30bfa9dd4daa7b64038edcdf77a
Malu Lauar <163319881+lauar-m@users.noreply.github.com>
2024-12-18 14:55:39 +0000

    add exercicios 1 e 2

---
+++ 1/dados.js
@@ -0,0 +1,12 @@
+function jogarDados(){
+    return Math.floor (Math.random() * 6 ) + 1 ;
+}
+
+const dado1 = jogarDados();
+const dado2 = jogarDados();
+
+const somar = dado1 + dado2;
+
+console.log(`Dado 1: ${dado1}`);
+console.log(`Dado 2: ${dado2}`);
+console.log(`Soma dos dados: ${somar}`);
---
+++ 2/mult_dados.js
@@ -0,0 +1,12 @@
+function jogarDados(){
+    return Math.floor (Math.random() * 6) + 1; 
+}
+for (let i = 0; i < 5; i++){
+    const dado1 = jogarDados();
+    const dado2 = jogarDados();
+    const somar = dado1 + dado2;
+
+    console.log(`Jogada: ${i} `);
+    console.log(`Dado 1: ${dado1}\nDado 2: ${dado2}\nSoma: ${somar}`);
+}
+
