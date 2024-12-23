aba95d6b2b97fc7800e0ea7138c1ad8a136aba7e
lauar-m <lauar.m@proton.me>
2024-12-23 11:43:28 -0300

    fix

--- 1/dados.js
+++ 1/dados.js
@@ -2,10 +2,10 @@ function jogarDados() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-const dado1 = jogarDados();
-const dado2 = jogarDados();
+let dado1 = jogarDados();
+let dado2 = jogarDados();
 
-const somar = dado1 + dado2;
+let somar = dado1 + dado2;
 
 console.log(`Dado 1: ${dado1}`);
 console.log(`Dado 2: ${dado2}`);
--- 3/enc_cond.js
+++ 3/enc_cond.js
@@ -4,23 +4,14 @@ function jogarDados() {
 
 /*Escreva um script para simular dois dados de 6 lados sendo lançados. O script deve “jogar” os dois dados até que a soma dos dois seja um número par ou maior que 10. 
 Para cada jogada, exibir o valor de cada dado individual e também exibir a soma dos dois dados.  */
-
+var jogada = 1;
 do {
-  const dado1 = jogarDados();
-  const dado2 = jogarDados();
-  const somar = dado1 + dado2;
-
+  let dado1 = jogarDados();
+  let dado2 = jogarDados();
+  var somar = dado1 + dado2;
+  console.log(`Jogada: ${jogada}`);
   console.log(`Dado 1: ${dado1}`);
   console.log(`Dado 2: ${dado2}`);
   console.log(`Soma dos dados: ${somar}`);
-  console.log("Jogando novamente...");
+  jogada++;
 } while (somar % 2 !== 0 && somar <= 10);
-{
-  console.log(`Dado 1: ${dado1}`);
-  console.log(`Dado 2: ${dado2}`);
-  console.log(`Soma dos dados: ${somar}`);
-  console.log("Jogando novamente...");
-  const dado1 = jogarDados();
-  const dado2 = jogarDados();
-  const somar = dado1 + dado2;
-}
